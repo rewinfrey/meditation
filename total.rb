88d228b14474111ddb6ed691cf1ee01c50bc48c2
Rick Winfrey <rewinfrey@github.com>
2019-01-24 11:01:15 -0800

    Update total script to break down by all time, year, and month

--- total.rb
+++ total.rb
@@ -1,12 +1,32 @@
 #! /usr/bin/env ruby
 
+require 'time'
+
 log = File.open("log.txt")
 
-total = log.inject(0) do |sum, line|
-  sum += line.split(" ").last.gsub("m", "").to_i
+totals = log.reduce({}) do |totals, line|
+  log_entry = line.split(" ")
+
+  date = Time.parse(log_entry.first)
+
+  minutes = log_entry.last.gsub("m", "").to_i
+
+  totals["All time"] ||= { total: 0 }
+  totals[date.year.to_s] ||= { total: 0 }
+  totals[date.year.to_s][date.month.to_s] ||= 0
+
+  totals["All time"][:total] += minutes
+  totals[date.year.to_s][:total] += minutes
+  totals[date.year.to_s][date.month.to_s] += minutes
+
+  totals
 end
 
-hours = total / 60
-minutes = total - (hours * 60)
-
-puts "Total meditation time: #{hours}h #{minutes}m"
+totals.keys.each do |key|
+  totals[key].keys.each do |sub_key|
+    total = totals[key][sub_key]
+    hours = total / 60
+    minutes = total - (hours * 60)
+    puts "#{key.to_s.ljust(10)} #{((sub_key.to_s) ++ ":").ljust(10)} #{hours}h #{minutes}m"
+  end
+end
